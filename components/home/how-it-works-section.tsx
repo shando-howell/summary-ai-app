9444f3199823ae9e013e4be27fb5d3e080c31b61
shandohowell <shandohowell94@gmail.com>
2025-03-23 00:43:34 -0500

    Working on cards for pricing

--- components/home/how-it-works-section.tsx
+++ components/home/how-it-works-section.tsx
@@ -55,8 +55,8 @@ const HowItWorksSection = () => {
 
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl mx-auto relative">
                 {steps.map((step, idx) => (
-                    <div className="relative flex items-stretch">
-                        <StepItem key={idx} {...step} />
+                    <div className="relative flex items-stretch" key={idx}>
+                        <StepItem {...step} />
 
                         {idx < steps.length - 1 && (
                             <div className='hidden md:block absolute top-1/2 -right-4 transform -translate-y-1/2 z-10'>
--- components/home/pricing-section.tsx
+++ components/home/pricing-section.tsx
@@ -1,6 +1,86 @@
+import Link from "next/link";
+
+type PriceType = {
+    name: string;
+    price: number;
+    description: string;
+    items: string[];
+    id: string;
+    paymentLink: string;
+    priceId: string;
+}
+
+const plans = [
+    {
+        name: 'Basic',
+        price: 9,
+        description: 'Perfect for occasional use',
+        items: [
+            '5 PDF summaries per month',
+            'Email support',
+        ],
+        id: 'basic',
+        paymentLink: '',
+        priceId: ''
+    },
+    {
+        name: 'Pro',
+        price: 19,
+        description: 'For professionals and teams',
+        items: [
+            'Unlimited PDF summaries',
+            'Priority processing',
+            '24/7 priority support',
+            'Markdown Export'
+        ],
+        id: 'pro',
+        paymentLink: '',
+        priceId: ''
+    },
+];
+
+const PricingCard = ({ name, price, description, items, id, paymentLink }: PriceType) => {
+    return (
+        <div className="relative w-full max-w-lg">
+            <div className='relative flex flex-col h-full gap-4 lg:gap-8'>
+                <div className="flex justify-between items-center gap-4">
+                    <div>
+                        <p className="text-lg lg:text-xl font-bold capitalize">{name}</p>
+                        <p className="text-base-content/80 mt-2">{description}</p>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <p>{price}</p>
+            </div>
+            <div>
+                {items.map((item, idx) => (
+                    <li key={idx}>{item}</li>
+                ))}
+            </div>
+            <div>
+                <Link href={paymentLink}>Buy Now</Link>
+            </div>
+        </div>
+    )
+};
+
 const PricingSection = () => {
   return (
-    <div>Pricing Section</div>
+    <section>
+        <div className="py-12 lg:py-24 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 lg:pt-12">
+            <div>
+                <h2>Pricing</h2>
+            </div>
+            <div className="relative flex justify-center flex-col lg:flex-row items-center
+            lg:items-stretch gap-8">
+                {plans.map((plan) => (
+                    <PricingCard key={plan.id} {...plan} />
+                ))}
+            </div>
+        </div>
+    </section>
   )
 }
 
