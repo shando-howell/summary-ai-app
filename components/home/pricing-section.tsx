0c931786edabdc1589d06b4abd05d6d9794de76d
shandohowell <shandohowell94@gmail.com>
2025-03-23 01:13:02 -0500

    Currently working on the pricing component.

--- components/home/pricing-section.tsx
+++ components/home/pricing-section.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 type PriceType = {
     name: string;
@@ -42,27 +43,26 @@ const plans = [
 const PricingCard = ({ name, price, description, items, id, paymentLink }: PriceType) => {
     return (
         <div className="relative w-full max-w-lg">
-            <div className='relative flex flex-col h-full gap-4 lg:gap-8'>
+            <div className={cn('relative flex flex-col h-full gap-4 lg:gap-8 z-10 p-8 border-[1px] border-gray-500/20 rounded-2xl',
+            id === 'pro' && 'border-blue-500 gap-5 border-2')}>
                 <div className="flex justify-between items-center gap-4">
                     <div>
                         <p className="text-lg lg:text-xl font-bold capitalize">{name}</p>
                         <p className="text-base-content/80 mt-2">{description}</p>
                     </div>
                 </div>
-            </div>
-
-            <div>
-                <p>{price}</p>
-            </div>
-            <div>
-                {items.map((item, idx) => (
-                    <li key={idx}>{item}</li>
-                ))}
-            </div>
-            <div>
-                <Link href={paymentLink}>Buy Now</Link>
-            </div>
-        </div>
+                        <div>
+                            <p>{price}</p>
+                        </div>
+            
+                        <div>
+                            {items.map((item, idx) => (
+                                <li key={idx}>{item}</li>
+                            ))}
+                        </div>
+                        <Link href={paymentLink}>Buy Now</Link>
+                    </div>
+                </div>
     )
 };
 
