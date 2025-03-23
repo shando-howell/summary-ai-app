71e530bb5d18b7ad4e5ec2a78ca691ab0b3f245d
shandohowell <shandohowell94@gmail.com>
2025-03-23 15:10:37 -0500

    Completed auth set up

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,13 @@
+import BgGradient from '@/components/common/bg-gradient';
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignIn />
+  return (
+    <section className="flex justify-center items-center lg:min-h-[40vh]">
+      <div className="py-12 lg:py-24 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 lg:pt-12">
+        <BgGradient className="from-blue-400 via-blue-300 to-green-300" />
+        <SignIn />
+      </div>
+    </section>
+  );
 }
