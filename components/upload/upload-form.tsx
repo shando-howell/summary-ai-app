0819bf35c775fd0d4d512b5a96d7475054fefcd9
shandohowell <shandohowell94@gmail.com>
2025-03-23 18:05:35 -0500

    Working on the file upload feature for app

--- components/upload/upload-form.tsx
+++ components/upload/upload-form.tsx
@@ -1,14 +1,39 @@
 "use client";
 
+import { z } from 'zod';
+
 import UploadFormInput from "@/components/upload/upload-form-input";
 
+const schema = z.object({ 
+    file: z
+        .instanceof(File, {message: 'Invalid file'})
+        .refine((file) => file.size <= 20 * 1024 * 1024, {
+            message: 'File size must be less than 20MB',
+        })
+        .refine((file) => file.type.startsWith('application/pdf'), 'File must be a PDF')
+});
+
 const UploadForm = () => {
-    const handleSubmit = () => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
         console.log("PDF submitted")
-    }
+        const formData = new FormData(e.currentTarget);
+        const file = formData.get('file') as File;
+
+        // validating the fields
+        // schema with zod
+        // upload the file to uploadthing
+        // parse the pdf using langchain
+        // summarize the PDF using AI
+        // save the summary to the database
+        // redirect to the [id] summary page
+    };
 
     return (
-        <UploadFormInput onSubmit={handleSubmit} />
+        <div className="flex flex-col gap-8 w-full max-w-2xl mx-auto">
+            <UploadFormInput onSubmit={handleSubmit} />
+        </div>
     )
 }
 
